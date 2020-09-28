75359c59e022bb3e5c4d2fe1dd50c26a48bb11d8
Roberto Montoya <roberto.montoya@alumnos.udg.mx>
2020-09-28 16:40:30 -0500

    prueba 01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    unsigned int base;
+    unsigned int altura;
+
+    cin >> base;
+    cin >> altura;
+
+    cout << (base * altura / 2)<<endl;
+
 
     return 0;
 }
